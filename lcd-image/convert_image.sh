bbdb6754bcd16d1d0ebaf1504e3bd6cdca408efe
Julien Vanier <julien@particle.io>
2023-06-15 14:39:11 -0400

    Fix endianness

--- lcd-image/convert_image.sh
+++ lcd-image/convert_image.sh
@@ -8,5 +8,5 @@ if [ $# -ne 2 ]; then
 fi
 
 stream -map rgb -storage-type char $1 - | \
-  perl -0777 -ne 'while ($pixel = substr($_, 0, 3, "")) { $red = ord(substr($pixel, 0, 1)); $green = ord(substr($pixel, 1, 1)); $blue = ord(substr($pixel, 2, 1)); $color = (($red >> 3) << 11) + (($green >> 2) << 5) + ($blue >> 3); print chr($color >> 8); print chr($color & 0xff) }' \
+  perl -0777 -ne 'while ($pixel = substr($_, 0, 3, "")) { $red = ord(substr($pixel, 0, 1)); $green = ord(substr($pixel, 1, 1)); $blue = ord(substr($pixel, 2, 1)); $color = (($red >> 3) << 11) + (($green >> 2) << 5) + ($blue >> 3); print chr($color & 0xff); print chr($color >> 8) }' \
   > $2
--- lcd-image/src/lcd-image.cpp
+++ lcd-image/src/lcd-image.cpp
@@ -8,8 +8,16 @@
 #include <Particle.h>
 #include <Adafruit_ILI9341.h>
 
-const auto TFT_DC = D5;
-const auto TFT_CS = D4;
+SYSTEM_THREAD(ENABLED);
+
+PRODUCT_VERSION(1);
+
+SerialLogHandler dbg(LOG_LEVEL_NONE, {
+  { "app", LOG_LEVEL_ALL }
+});
+
+const auto TFT_DC = S4;
+const auto TFT_CS = S3;
 
 Adafruit_ILI9341 tft(TFT_CS, TFT_DC);
 
@@ -20,10 +28,11 @@ void setup() {
   auto assets = System.assetsAvailable();
   for (auto& asset: assets) {
     if (asset.name() == "splash.img") {
+      LOG(INFO, "Found splash.img");
       // draw the splash screen line by line
-      for (int y = 0; y < ILI9341_TFTHEIGHT; y++) {
+      for (int x = 0; x < ILI9341_TFTWIDTH; x++) {
         // each pixel is a 16 bit value, with the RGB values packed 5 bits for red, 6 bits for green, 5 bits for blue
-        uint16_t line[ILI9341_TFTWIDTH] = { 0 };
+        uint16_t line[ILI9341_TFTHEIGHT] = { 0 };
         char* buf = (char*) line;
         int read = 0;
         while (asset.available() && read < (int)sizeof(line)) {
@@ -31,8 +40,12 @@ void setup() {
         }
 
         // draw the line
-        tft.drawRGBBitmap(0, y, line, ILI9341_TFTWIDTH, 1);
+        tft.drawRGBBitmap(ILI9341_TFTWIDTH - x - 1, 0, line, 1, ILI9341_TFTHEIGHT);
+
+        // curtain effect
+        delay(10);
       }
+      LOG(INFO, "Done drawing splash.img");
     }
   }
 }
